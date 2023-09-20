c8e5d1c0eeb95c3874ff93c5e3e66658fe532f5d
V3dr0n1 <jv.vedroni@gmail.com>
2023-09-20 11:26:43 -0300

    deleteById test cases

---
+++ tests/cidades/GetAll.test.ts
@@ -0,0 +1,18 @@
+import { StatusCodes } from "http-status-codes";
+import { testServer } from "../jest.setup"
+
+describe('Cidades - GetAll', async() => {
+  const rest1 = await testServer
+    .post('/cidades')
+    .send({nome: 'São PAulo'});
+  
+  expect(rest1.statusCode).toEqual(StatusCodes.CREATED);
+
+  const resBuscada = await testServer
+    .get('/cidades')
+    .send();
+
+  expect(Number(resBuscada.header['x-total-count'])).toBeGreaterThan(0);
+  expect(resBuscada.statusCode).toEqual(StatusCodes.Ok);
+  expect(resBuscada.body.length)
+})
